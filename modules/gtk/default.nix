3be16dd7a671a85ba49e73cc579901d2cb9814fb
Adithya K B <adithyakb93@gmail.com>
2024-11-06 22:53:16 +0530

    Configured icons and cursor theme

--- modules/gtk/default.nix
+++ modules/gtk/default.nix
@@ -2,11 +2,22 @@
 
   gtk = {
     enable = true;    
+    # gtk theme
     theme.package = (pkgs.graphite-gtk-theme.override {
       tweaks = [ "nord" ] ;
       colorVariants = [ "dark" ];
     });
     theme.name = "Graphite-Dark-nord";
+    # gtk icon theme
+    iconTheme.package = pkgs.dracula-icon-theme;
+    iconTheme.name = "Dracula";
+    # gtk cursor theme
+    cursorTheme.package = pkgs.rose-pine-cursor;
+    cursorTheme.name = "BreezeX-RosePineDawn-Linux";
+    cursorTheme.size = 24;
   };
-
+  # setting home cursor
+  home.pointerCursor.package = pkgs.rose-pine-cursor;
+  home.pointerCursor.name = "BreezeX-RosePineDawn-Linux";
+  home.pointerCursor.size = 24;
 }
