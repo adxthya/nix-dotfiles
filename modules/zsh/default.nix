6d974297f0011ab39ddcc0465b590b00746c91e3
Adithya K B <adithyakb93@gmail.com>
2024-11-06 18:59:15 +0530

    Modularized zsh config

---
+++ modules/zsh/default.nix
@@ -0,0 +1,34 @@
+{ pkgs }:{
+
+  programs.zsh.enable = true;
+  programs.zsh = {
+    syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "v0.4.0";
+        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+        };
+      }
+    ];
+    shellAliases = {
+      c="clear";
+      config="sudo nano ~/dotfiles/configuration.nix";
+      build="sudo nixos-rebuild switch --flake ~/dotfiles/";
+      home="nano ~/dotfiles/home.nix";
+      home-build="home-manager switch --flake ~/dotfiles/";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme="robbyrussell";
+      plugins = [
+        "git"
+        "sudo"
+      ];
+    };
+  };
+
+}
--- users/adxthya/home.nix
+++ users/adxthya/home.nix
@@ -6,44 +6,16 @@
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
+  imports = [
+    ../../modules
+  ];
+
   home.packages = with pkgs;[
     zsh
     stow
     slurp
     grim
   ];
-
-  # Zsh Config
-  programs.zsh.enable = true;
-  programs.zsh = {
-    syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-autosuggestions";
-        rev = "v0.4.0";
-        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-        };
-      }
-    ];
-    shellAliases = {
-      c="clear";
-      config="sudo nano ~/dotfiles/configuration.nix";
-      build="sudo nixos-rebuild switch --flake ~/dotfiles/";
-      home="nano ~/dotfiles/home.nix";
-      home-build="home-manager switch --flake ~/dotfiles/";
-    };
-    oh-my-zsh = {
-      enable = true;
-      theme="robbyrussell";
-      plugins = [
-        "git"
-        "sudo"
-      ];
-    };
-  };
 
 
   home.file = {
