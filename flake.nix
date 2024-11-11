1a271bf00f3d6d03bc5a9daf033c7aeb12df0f36
Adithya K B <adithyakb93@gmail.com>
2024-11-11 11:55:03 +0530

    Updated flakes

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "Home Manager configuration of adxthya";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # Remove this when zen gets added to nixpkgs
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,6 +26,7 @@
       };
       homeConfigurations = {
         adxthya = home-manager.lib.homeManagerConfiguration {
+          extraSpecialArgs = {inherit inputs;};
           inherit pkgs;
           modules = [ ./users ];
         };
