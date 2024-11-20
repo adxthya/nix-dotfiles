cfa0f3f27e1d060afc420b560e72221d8002acfa
adxthya <adithyakb93@gmail.com>
2024-11-20 11:46:08 +0530

    Add dunst config to modules

--- modules/dunst/default.nix
+++ modules/dunst/default.nix
@@ -1,44 +1,34 @@
 { pkgs, lib, config, ... }:
+{
+  home.packages = with pkgs; [
+    dunst
+  ];
 
-with lib;
-let
-  cfg = config.modules.dunst;
-in {
-  options.modules.dunst = {
-    enable = mkEnableOption "dunst";
-  };
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        origin = "top-right";
+        offset = "60x12";
+        separator_height = 2;
+        padding = 12;
+        horizontal_padding = 12;
+        text_icon_padding = 12;
+        frame_width = 4;
+        separator_color = "frame";
+        idle_threshold = 120;
+        font = "FiraCode Nerdfont 10";
+        line_height = 0;
+        format = "<b>%s</b>\n%b";
+        alignment = "center";
+        icon_position = "off";
+        startup_notification = "false";
+        corner_radius = 12;
 
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      dunst
-    ];
-
-    services.dunst = {
-      enable = true;
-      settings = {
-        global = {
-          origin = "top-left";
-          offset = "60x12";
-          separator_height = 2;
-          padding = 12;
-          horizontal_padding = 12;
-          text_icon_padding = 12;
-          frame_width = 4;
-          separator_color = "frame";
-          idle_threshold = 120;
-          font = "JetBrainsMono Nerdfont 12";
-          line_height = 0;
-          format = "<b>%s</b>\n%b";
-          alignment = "center";
-          icon_position = "off";
-          startup_notification = "false";
-          corner_radius = 12;
-
-          frame_color = "#44465c";
-          background = "#303241";
-          foreground = "#d9e0ee";
-          timeout = 2;
-        };
+        frame_color = "#44465c";
+        background = "#303241";
+        foreground = "#d9e0ee";
+        timeout = 2;
       };
     };
   };
